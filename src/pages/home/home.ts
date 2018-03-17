c3ea985917e8f6e8341a9276576b51e201ff175c
otaviotarelhodb <otarelho@ucdavis.edu>
2018-03-16 21:39:06 -0300

    Auto login when has a not expired token

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -30,6 +30,13 @@ export class HomePage {
     this.menu.swipeEnable(true);
   }
 
+  ionViewDidEnter(){
+    this.auth.refreshToken().subscribe(response => {
+      this.auth.successfulLogin(response.headers.get("Authorization"));
+      this.navCtrl.setRoot("CategoriasPage");
+    }, error => {});
+  }
+
   public login(){
     this.auth.authenticate(this.credenciais).subscribe(response => {
       this.auth.successfulLogin(response.headers.get("Authorization"));
