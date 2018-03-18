cf2a74bcd5459059744b416d709172b2c4cb261d
otaviotarelhodb <otarelho@ucdavis.edu>
2018-03-18 19:17:07 -0300

    Access to cart through menu and float button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,7 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: "ProfilePage" },
       { title: 'Categorias', component: "CategoriasPage" },
+      { title: 'Cart', component: "CartPage" },
       { title: 'Logout', component: "" }
     ];
 
