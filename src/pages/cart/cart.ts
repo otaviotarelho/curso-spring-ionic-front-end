1328cee4b8dbbc2f3412776d547c3193a3ab8d12
otaviotarelhodb <otarelho@ucdavis.edu>
2018-03-18 19:02:59 -0300

    Finishing Cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../../models/cartItem';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 /**
  * Generated class for the CartPage page.
@@ -45,4 +46,23 @@ export class CartPage {
     }
   }
 
+  removeItem(produto: ProdutoDTO){
+    this.items = this.cart.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO){
+    this.items = this.cart.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cart.decreaseQuantity(produto).items;
+  }
+
+  total(): number {
+    return this.cart.total();
+  }
+
+  goOn(){
+    this.navCtrl.setRoot("CategoriasPage");
+  }
 }
